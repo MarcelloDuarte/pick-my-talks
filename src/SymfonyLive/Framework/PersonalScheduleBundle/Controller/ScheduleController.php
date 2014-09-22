8251a3923a4017832e78f44315629a19d4f72e0b
everzet <ever.zet@gmail.com>
2014-09-22 23:49:54 +0100

    Write personal schedule template and update conferenceAction

--- src/SymfonyLive/Framework/PersonalScheduleBundle/Controller/ScheduleController.php
+++ src/SymfonyLive/Framework/PersonalScheduleBundle/Controller/ScheduleController.php
@@ -3,9 +3,10 @@
 namespace SymfonyLive\Framework\PersonalScheduleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Request;
+use SymfonyLive\Conference\Conference;
 
 class ScheduleController extends Controller
 {
@@ -15,9 +16,10 @@ class ScheduleController extends Controller
      */
     public function conferenceAction($conferenceName)
     {
-        $conference = $this->getConferenceByNameOr404($conferenceName);
+        $conference = $this->getConferenceByNameOr404(urldecode($conferenceName));
+        $mySchedule = $this->getConferenceSchedule($conference);
 
-        return ['conference' => $conference];
+        return ['conference' => $conference, 'mySchedule' => $mySchedule];
     }
 
     /**
@@ -26,27 +28,23 @@ class ScheduleController extends Controller
      */
     public function chooseTalkAction(Request $request, $conferenceName, $talkName)
     {
-        $conference = $this->getConferenceByNameOr404($conferenceName);
-
         return $this->redirect($request->headers->get('referer'));
     }
 
-    /**
-     * @param $conferenceName
-     *
-     * @return mixed
-     */
-    private function getConferenceByNameOr404($conferenceName)
+    private function getConferenceByNameOr404($name)
     {
-        $conferenceName = urldecode($conferenceName);
-        $conference = $this->get('symfony_live.conference_repository')->findConference(
-            $conferenceName
-        );
+        $conference = $this->get('symfony_live.conference_repository')->findConference($name);
 
         if (!$conference) {
             throw $this->createNotFoundException();
         }
 
         return $conference;
+    }
+
+    private function getConferenceSchedule(Conference $conference)
+    {
+        return $this->get('symfony_live.personal_schedule_repository')
+            ->findOrCreateConferenceSchedule($conference);
     }
 }
