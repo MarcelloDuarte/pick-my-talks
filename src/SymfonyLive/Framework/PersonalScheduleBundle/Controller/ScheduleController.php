e45f645bfecbe93c2609f958983f2fa13d2249e7
everzet <ever.zet@gmail.com>
2014-09-22 23:31:02 +0100

    Fix controller search of the conference

--- src/SymfonyLive/Framework/PersonalScheduleBundle/Controller/ScheduleController.php
+++ src/SymfonyLive/Framework/PersonalScheduleBundle/Controller/ScheduleController.php
@@ -14,6 +14,7 @@ class ScheduleController extends Controller
      */
     public function conferenceAction($name)
     {
+        $name = urldecode($name);
         $conference = $this->get('symfony_live.conference_repository')->findConference($name);
 
         if (!$conference) {
